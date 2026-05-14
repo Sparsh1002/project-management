03769426abc2071892f9e36861ade6fce261a2ad
Sparsh Lodha <sparshlodha04@gmail.com>
2026-05-14 12:37:33 +0530

    Removed passlib

--- app/utils/security.py
+++ app/utils/security.py
@@ -2,7 +2,6 @@ from datetime import datetime, timedelta, timezone
 import os
 import bcrypt
 from jose import jwt
-from passlib.context import CryptContext
 from fastapi.security import HTTPBearer
 
 
@@ -15,10 +14,6 @@ ALGORITHM = os.getenv("ALGORITHM")
 ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES"))
 
 security = HTTPBearer()
-pwd_context = CryptContext(
-    schemes=["bcrypt"],
-    deprecated="auto"
-)
 
 
 def hash_password(password: str) -> str:
